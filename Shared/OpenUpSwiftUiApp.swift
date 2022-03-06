f697d63743b01e1ce405d075a0957f3668576549
Manas Katyal <73521088+msk-stack@users.noreply.github.com>
2022-03-06 20:31:50 +0100

    Initial Commit

---
+++ Shared/OpenUpSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenUpSwiftUiApp.swift
+//  Shared
+//
+//  Created by Manas Singh Katyal on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct OpenUpSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
