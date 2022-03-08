d1a9cd3736d480230d58745a077ab61b06582896
Manas Katyal <73521088+msk-stack@users.noreply.github.com>
2022-03-08 01:13:29 +0100

    3 Views added
    MainView, LikedView & AboutView.
    
    Now the upcoming - is to create the views.
    
    WKWebkit for the about view.
    LikedView needs a TableView with information from the Keyboard to the DataTable so you can see all your liked icebreakers
    
    and then the home TAB where you get guided into how to activate the keyboard

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -8,32 +8,95 @@
 import Combine
 import SwiftUI
 
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.red
+                        
+            }
+            .navigationTitle("Home")
+        }
+       
+    }
+}
+    
+
+struct AboutView: View {
+    var body: some View {
+        NavigationView {
+            
+            ZStack {
+                Color.blue
+        
+            }
+            .navigationTitle("About")
+        }
+    }
+    
+    struct LikedView: View {
+        var body: some View {
+            NavigationView {
+                
+                ZStack {
+                    Color.purple
+            
+                }
+                .navigationTitle("Liked icebreakers")
+            }
+        }
+
 struct ContentView: View {
     var body: some View {
+        TabView {
+            
+            //Home View
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            //Liked View in Tab
+            LikedView()
+                .tabItem {
+                    Image(systemName: "heart")
+                    Text("Liked Icebreakers")
+        }
+            //About View in Tab
+            AboutView()
+                .tabItem {
+                    Image(systemName: "person.2")
+                    Text("About")
+                }
+                
 
             
-        VStack(alignment: .leading) {
-            Text("OpenUp Keyboard")
-                .font(.largeTitle)
-                .bold()
-                .frame(width: 300, height: 30, alignment: .leading)
-            Text("Version 1.0")
-            Text("INSTALLATION")
-                .font(.bold(.subheadline)())
+   //     VStack(alignment: .leading) {
+    //        Text("OpenUp Keyboard")
+      //          .font(.largeTitle)
+        //       .bold()
+          //      .frame(width: 300, height: 30, alignment: .leading)
+           // Text("Version 1.0")
+            //Text("INSTALLATION")
+              //  .font(.bold(.subheadline)())
             
-            Button("Go to Settings") {
-                UIApplication.shared.open(URL(string: "App-prefs:General&path=Keyboard")!)
+          //  Button("Go to Settings") {
+            //    UIApplication.shared.open(URL(string: "App-prefs:General&path=Keyboard")!)
             }
              
         }
-        }
 
-    }
+    
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
 .previewInterfaceOrientation(.portrait)
+    }
+}
+
+}
     }
 }
